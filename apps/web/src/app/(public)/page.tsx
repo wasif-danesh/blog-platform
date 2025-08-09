8b3346660fbb4b342cd057d53827971ee171dedd
Wasif Danesh <wasif.danesh@gmail.com>
2025-08-10 04:44:45 +1000

    feature(web): bypass static prerender

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,3 +1,7 @@
+// Render at request time to bypass static prerender glitches on Vercel
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import PublicGrid from './PublicGrid'; // client component
 
 export default function Home() {
